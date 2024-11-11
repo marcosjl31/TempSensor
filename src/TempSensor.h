36fa8af6cb1896845f488332e497a3e94f9a8463
José L. Marcos <marcosjl31@gmail.com>
2024-11-11 17:20:22 +0100

    Code modified to reead Temp/humi from AHT20, and read batt level

---
+++ src/TempSensor.h
@@ -0,0 +1,10 @@
+#ifndef TEMPSENSOR_H
+#define TEMPSENSOR_H
+
+#include <Arduino.h>
+#include <AHT20.h>
+
+// Objects to manipulate AHT20 sensor
+AHT20 aht20;
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,63 @@
 #include <Arduino.h>
+#include "TempSensor.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN,OUTPUT);
+float volt;
+float calib_factor = 5.18; // change this value to calibrate the battery voltage
+int batterypercentage;
+
+void setup()
+{
   Serial.begin(115200);
-  Serial.println("Setup done.");
+  Serial.println("Humidity AHT20 examples");
+
+  Wire.begin(); //Join I2C bus
+
+  //Check if the AHT20 will acknowledge
+  if (aht20.begin() == false)
+  {
+    Serial.println("AHT20 not detected. Please check wiring. Freezing.");
+    while (1);
+  }
+  Serial.println("AHT20 acknowledged.");
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN,LOW);
-  Serial.println("Led is ON");
-  delay(1000);
-  digitalWrite(LED_BUILTIN,HIGH);
-  Serial.println("Led is OFF");
-  delay(1000);
+void loop()
+{
+  //If a new measurement is available
+  if (aht20.available() == true)
+  {
+    //Get the new temperature and humidity value
+    float temperature = aht20.getTemperature();
+    float humidity = aht20.getHumidity();
+
+    //Print the results
+    Serial.print("Temperature: ");
+    Serial.print(temperature, 2);
+    Serial.print(" C\t");
+    Serial.print("Humidity: ");
+    Serial.print(humidity, 2);
+    Serial.print("% RH");
+
+    Serial.println();
+  }
+
+  //The AHT20 can respond with a reading every ~50ms. However, increased read time can cause the IC to heat around 1.0C above ambient.
+  //The datasheet recommends reading every 2 seconds.
+  delay(2000);
+
+  // Voltage divider R1 = 220k+100k+220k =540k and R2=100k
+
+  unsigned long raw = analogRead(A0);
+  volt = raw * calib_factor / 1024;
+
+  Serial.print("Voltage = ");
+  Serial.print(volt, 2); // print with 2 decimal places
+  Serial.println (" V");
+
+  batterypercentage = (volt - 3.6) * 100 / 0.6;   // 3.6 V is the lower limint set to 0%, bandwith 0.6 V
+  if (batterypercentage > 100) batterypercentage = 100;
+  Serial.print("Battery charge: ");
+  Serial.print(batterypercentage);
+  Serial.println("%");
+
 }
